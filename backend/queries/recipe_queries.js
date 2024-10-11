a9567d11f1eb2c18165e2c386ce5e2bb09f33fb3
XSquid <102339058+XSquid@users.noreply.github.com>
2024-10-11 15:23:05 -0600

    Finished search by tag functionality

--- backend/queries/recipe_queries.js
+++ backend/queries/recipe_queries.js
@@ -1,5 +1,6 @@
 const {database} = require('../database')
 
+//Retrieve all recipes
 const getAllRecipes = (request, response) => {
     database.query('SELECT * FROM RECIPES', (error, results) => {
         if (error) {
@@ -9,6 +10,7 @@ const getAllRecipes = (request, response) => {
     })
 }
 
+//Retrieve single recipe
 const getRecipe = (request, response) => {
     const id = parseInt(request.params.id)
     database.query('SELECT * FROM recipes WHERE id = $1', [id], (error, results) => {
@@ -19,9 +21,11 @@ const getRecipe = (request, response) => {
     })
 }
 
+//Add recipe to database
+//Need to add checks for malicious code
+//Need to add checks for duplicates
 const addRecipe = (request, response) => {
     const {recipe_name, ingredients, steps, additional, tags} = request.body
-
     database.query('INSERT INTO recipes (name, ingredients, steps, instructions, tags) VALUES ($1, $2, $3, $4, $5)',
         [recipe_name, ingredients, steps, additional, tags], (error, results) => {
             if (error) {
@@ -35,16 +39,52 @@ const addRecipe = (request, response) => {
     )
 }
 
-const searchByName = (req, res) => {
-    const recipeSearch = `%${req.query.q}%`
-    database.query('SELECT * FROM recipes WHERE name ILIKE $1', [recipeSearch], (error, results) => {
+const searchForRecipe = (req, res) => {
+    //Search by name
+    if (req.query.q) {
+        const recipeSearch = `%${req.query.q}%`
+        database.query('SELECT * FROM recipes WHERE name ILIKE $1', [recipeSearch], (error, results) => {
+            if (error) {
+                console.log (`Error: ${error}`)
+                res.sendStatus(500)
+                return null
+            }
+            return res.status(200).json(results.rows)
+        })
+    }
+    //Search by tag
+    if (req.query.tag) {
+        //Need to ensure recipeSearch is an array, when only one tag is provided it is not in an array. Put into array tags and flattened to ensure recipeSearch is always a 1d array
+        const recipeSearch = [req.query.tag].flat() 
+        console.log(recipeSearch)
+        database.query('SELECT * FROM RECIPES WHERE tags @> $1', [recipeSearch], (error, results) => {
+            if (error) {
+                console.log (`Error: ${error}`)
+                res.sendStatus(500)
+                return null
+            }
+            return res.status(200).json(results.rows)
+        })
+    }
+}
+
+//Return all unique tags in database
+const getUniqueTags = (req, res) => {
+    database.query('SELECT DISTINCT tags FROM recipes', (error, results) => {
         if (error) {
             console.log (`Error: ${error}`)
             response.sendStatus(500)
             return null
         }
-        console.log(results.rows)
-        return res.status(200).json(results.rows)
+        //Results is tags for every column, need to only keep unique tags and flatten array
+        function onlyUnique(value, index, array) {
+            return array.indexOf(value) === index;
+          }
+        const tags = []
+        results.rows.forEach((ea) => tags.push(ea.tags))
+        const flatTags = tags.flat()
+        const uniqueTags = flatTags.filter(onlyUnique)
+        return res.status(200).json(uniqueTags)
     })
 }
 
@@ -52,5 +92,6 @@ module.exports = {
     getAllRecipes,
     addRecipe,
     getRecipe,
-    searchByName
+    searchForRecipe,
+    getUniqueTags
 }
