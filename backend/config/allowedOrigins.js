a5e8819627a327545b084794a3cfbf73bf2b49da
XSquid <102339058+XSquid@users.noreply.github.com>
2024-11-04 01:20:18 -0600

    updated cors allowedorigins

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,9 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    "http://localhost:3001"
+    "http://localhost:3001",
+    'http://localhost:10000',
+    'http://0.0.0.0:3000',
+    'http://0.0.0.0:10000'
 ]
 
 module.exports = allowedOrigins
