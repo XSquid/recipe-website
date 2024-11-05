c4f40814ca87c420281f06479709a146312c6a83
XSquid <102339058+XSquid@users.noreply.github.com>
2024-11-05 14:10:00 -0600

    cors update

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -11,7 +11,7 @@ const allowedOrigins = [
     '35.164.95.156',
     '44.230.95.183',
     '44.229.200.200',
-    'https://recipe-book-frontend-od7o.onrender.com/',
+    'https://recipe-book-frontend-od7o.onrender.com',
     'https://recipe-book-frontend-od7o.onrender.com/:10000'
 ]
 
