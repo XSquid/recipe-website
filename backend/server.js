ccefcf132d329e09d3bfc6cef25f447d6be324f4
XSquid <102339058+XSquid@users.noreply.github.com>
2024-11-05 14:51:24 -0600

    Changed cookie secure to true, trying to fix problems with render deployment

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: false,
+        secure: true,
         maxAge: 1000 * 60 * 10 // 10 minutes
     }
 }))
